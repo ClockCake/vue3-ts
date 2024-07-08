9c936465540e259f1d404b9a1492bc2bddf41752
ClockCat <hyd1234@outlook.com>
2024-07-08 17:50:51 +0800

    feat: login UI

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,18 @@
 <template>
   <div id="app">
-  <el-row>
-    <el-col :span="24"><div class="firstRow">
-      <el-button>Default</el-button>
-      <el-button type="primary">Primary</el-button>
-      <el-button type="success">Success</el-button>
-      <el-button type="info">Info</el-button>
-      <el-button type="warning">Warning</el-button>
-      <el-button type="danger">Danger</el-button>
-    </div></el-col>
-  </el-row>
+    <login-in class="login"></login-in>
   </div>
 </template>
 
 
 <script>
+import Login from './components/login.vue';
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    LoginIn: Login
+  },
 };
 </script>
 
@@ -25,25 +21,16 @@ html, body {
   margin: 0;
   padding: 0;
 }
-/* #app {
-  background-color: white;
-  width: 100vw;
+#app {
+  /* 渐变色背景 */
+  background: linear-gradient(45deg, #49a09d, #5f2c82);
+  /* 全屏 */
   height: 100vh;
+  width: 100vw;
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
 
-} */
-.firstRow {
-  background-color: lightgray;
-  border-radius: 4px;
-  min-height: 48px;
-  margin: 20px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  
-  
 }
+
 </style>
---
+++ src/components/login.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="login">
+        <!-- element UI -->
+         <h2>登录</h2>
+        <el-form ref="form" :model="form" label-width="80px">
+            <el-form-item label="用户名" prop="username">
+                <el-input v-model="form.username"></el-input>
+            </el-form-item>
+            <el-form-item label="密码" prop="password">
+                <el-input type="password" v-model="form.password"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="login">登录</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      form: {
+        username: '',
+        password: '',
+      },
+    };
+  },
+};
+</script>
+
+<style>
+.login {
+  width: 80%; /* 使用百分比宽度，根据屏幕宽度自适应 */
+  max-width: 400px; /* 设置最大宽度，避免在大屏设备上过宽 */
+  /* margin: 20px auto; 居中显示 */
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 5px;
+}
+
+h2 {
+  text-align: center;
+  margin-bottom: 20px;
+}
+.el-button {
+    width: 40%;
+    padding: 20px;
+}
+
+</style>
