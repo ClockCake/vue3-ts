7309611b34925ea6ee0e095114096aaabc711a9f
ClockCat <hyd1234@outlook.com>
2024-07-15 14:42:41 +0800

    feat: 完成会员特权页面 UI

--- src/components/memberlevel.vue
+++ src/components/memberlevel.vue
@@ -1,15 +1,35 @@
 <template>
     <div class="bgview">
         <img class="bgv" src="../assets/bgv.png" alt="">
-        <div class="infoStack">
-            <img class="avator" :src="avator" alt="">
-            <div class="nameStack">
-                <div>{{ name }}</div>
-                <div>{{ level }}</div>
+        <div class="fullVStack">
+            <div class="infoStack">
+                <img class="avatar" :src="avatar" alt="">
+                <div class="nameVStack">
+                    <div class="levelHStack">
+                        <span id="name">{{ name }}</span>
+                        <img id="level" src="../assets/level.png" alt="">
+                    </div>
+                    <div>
+                        <progress :value="progress" max="1"></progress>                    
+                    </div>
+                    <div class="desc">
+                        <span>{{ desc }}</span>
+                        <span>V.2</span>
+                    </div>
+                </div>
+            </div>
+            <div class="memberPrivileges">
+                <h2>会员特权</h2>
+                <div class="privilegesGrid">
+                    <div v-for="(membership, index) in memberships" :key="index" class="privilegeItem">
+                        <img :src="membership.image" :alt="membership.name">
+                        <h3>{{ membership.name }}</h3>
+                        <p>{{ membership.desc }}</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-
 </template>
 
 
@@ -23,11 +43,64 @@ export default {
     },
     data() {
         return {
-            avator: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/18.jpg',
+            avatar: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/18.jpg',
             name: '超级会员',
-            level: 0,
-            progress: 0.3,
+            level: 1,
+            progress: 0.5,
             desc: '还需200积分可升级',
+            memberships: [
+                {
+                    image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/18.jpg',
+                    name: '维修检测',
+                    desc: '免费水电基础维修检测',
+                },
+                {
+                    image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/18.jpg',
+                    name: '维修检测',
+                    desc:  '免费水电基础维修检测',
+                },
+                {
+                    image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/18.jpg',
+                    name: '维修检测',
+                    desc: '免费水电基础维修检测',
+                },
+                {
+                    image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/18.jpg',
+                    name: '维修检测',
+                    desc: '免费水电基础维修检测',
+                },
+                {
+                    image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/18.jpg',
+                    name: '维修检测',
+                    desc: '免费水电基础维修检测',
+                },
+                {
+                    image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/18.jpg',
+                    name: '维修检测',
+                    desc: '免费水电基础维修检测',
+                },
+                {
+                    image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/18.jpg',
+                    name: '维修检测',
+                    desc: '免费水电基础维修检测',
+                },
+                {
+                    image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/18.jpg',
+                    name: '维修检测',
+                    desc: '免费水电基础维修检测',
+                },
+                {
+                    image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/18.jpg',
+                    name: '维修检测',
+                    desc: '免费水电基础维修检测',
+                },
+                {
+                    image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/18.jpg',
+                    name: '维修检测',
+                    desc: '免费水电基础维修检测',
+                },
+                
+            ]
         }
     },
 };
@@ -51,21 +124,119 @@ body {
     position: absolute;
     top: 0;
     right: 0;
+
+}
+.fullVStack {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    position: relative; 
+    z-index: 1; 
 }
 .infoStack {
     display: flex;
-    position: absolute;
-    direction: horizontal;
-    justify-content: center;
+    margin-top: v-bind('px2rem(64)');
+    flex-direction: row;
+    justify-content: flex-start;
     top: v-bind('px2rem(72)');
     width: 100%;
+    padding-right: v-bind('px2rem(20)'); /* 添加右边距 */
+    box-sizing: border-box; /* 确保padding不会增加总宽度 */
 
 }
-.avator {
+.avatar {
     width: v-bind('px2rem(54)');
     height: v-bind('px2rem(54)');
     border-radius: 50%;
+    margin-left: v-bind('px2rem(20)');
+}
+.nameVStack {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    margin-left: v-bind('px2rem(20)');
+    width: calc(100% - v-bind('px2rem(94)')); /* 减去头像宽度和边距 */
+
+}
+.levelHStack {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+}
+#name{
+    color: white;
+    /* 中粗体 15pt */
+    font: normal normal 15px/18px PingFang SC;
+}
+#level {
+    width: v-bind('px2rem(50)');
+    height: v-bind('px2rem(20)');
+    margin-left: v-bind('px2rem(10)');
+}
+progress {
+    width: 100%;
+    height: v-bind('px2rem(5)');
+    border-radius: v-bind('px2rem(2.5)');
+    background-color: #66625C;
+
+}
+/* 填充色（进度条颜色） */
+progress::-webkit-progress-value {
+    background-color: #FACEA3; /* 设置您想要的填充颜色 */
+    border-radius: v-bind('px2rem(2.5)');
+}
+.desc {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-top: v-bind('px2rem(10)');
+    color: white;
+    font: normal normal 12px/14px PingFang SC;
 }
 
+.memberPrivileges {
+    margin-top: v-bind('px2rem(30)');
+    padding: v-bind('px2rem(20)');
+    overflow-y: auto; /* 启用垂直滚动 */
+    background-color: white;
+    border-radius: v-bind('px2rem(20)') v-bind('px2rem(20)') 0 0;
+    flex: 1; /* 让它占据 fullVStack 中的剩余空间 */
+    display: flex;
+    flex-direction: column;
+}
 
+.memberPrivileges h2 {
+    color: #FACEA3;
+    text-align: center;
+    font-size: v-bind('px2rem(18)');
+    margin-bottom: v-bind('px2rem(20)');
+}
+
+.privilegesGrid {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: v-bind('px2rem(20)');
+}
+
+.privilegeItem {
+    background-color: rgba(255, 255, 255, 0.1);
+    text-align: center;
+}
+
+.privilegeItem img {
+    width: v-bind('px2rem(83)');
+    height: v-bind('px2rem(83)');
+}
+
+.privilegeItem h3 {
+    color: #333333 100%;
+    font-size: v-bind('px2rem(12)');
+    font-bold: bold;
+    margin-bottom: v-bind('px2rem(5)');
+}
+
+.privilegeItem p {
+    color: #999999;
+    font-size: v-bind('px2rem(10)');
+}
 </style>
