4cf609507e8272e8565363a28b3d14b4a1cf6d45
ClockCat <hyd1234@outlook.com>
2024-07-16 17:50:58 +0800

    feat: 邀请用户获得积分，未完待续

--- src/components/getScore.vue
+++ src/components/getScore.vue
@@ -1,11 +1,63 @@
 <template>
-
+<div class="warp">
+    <div class="image-container">
+        <img src="../assets/scoreBV.png" alt="" class="background-image">
+        <div class="overlay">
+            <!-- 这里放置你想要添加的内容 -->
+             <div>邀请用户领积分</div>
+             <img src="../assets/RedPocket.png" alt="">
+        </div>
+    </div>
+    <div class="middle"></div>
+    <div class="bottomList"></div>
+</div>
 </template>
 
 <script lang="ts">
+import { px2rem } from '../utils/rem'
 
+export default {
+    name: 'GetScore',
+    setup() {
+     return { px2rem }
+    },
+    
+}
 </script>
 
-<style>
+<style scoped>
+.warp {
+    /* 渐变色背景 */
+    background: white;
+    /* 全屏 */
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    justify-content: flex-start;
+}
 
+.image-container {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  /* 添加其他样式，如背景色、透明度等 */
+}
+.overlay div {
+    font-size: v-bind('px2rem(16)');
+    color: black;
+    margin-top: 100px;
+    text-align: center;
+}
 </style>
--- src/router.js
+++ src/router.js
@@ -1,11 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MemberLevel from './components/memberlevel.vue'
-
+import GetScore from './components/getScore.vue'
 const routes = [
   {
     path: '/',
     name: 'MemberLevel',
     component: MemberLevel
+  },
+  {
+    path: '/invite',
+    name: 'GetScore',
+    component: GetScore
   },
 ]
 
